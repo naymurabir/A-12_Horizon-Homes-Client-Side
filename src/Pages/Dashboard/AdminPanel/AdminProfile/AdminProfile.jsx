1d02ecf3197b229ebf5de1bc41e9e8eed7d55809
naymurabir <naymurrahman.abir@gmail.com>
2023-11-29 20:57:39 +0600

    user profile agent profile and admin profile has been done

--- src/Pages/Dashboard/AdminPanel/AdminProfile/AdminProfile.jsx
+++ src/Pages/Dashboard/AdminPanel/AdminProfile/AdminProfile.jsx
@@ -1,8 +1,40 @@
+import useAuth from "../../../../Hooks/useAuth";
+import useUsers from "../../../../Hooks/useUsers";
 
 const AdminProfile = () => {
+    const { user } = useAuth()
+
+    const { users } = useUsers()
+
+    const userRole = users.find(user1 => user1.email === user?.email)
+    console.log(userRole);
+
     return (
-        <div>
-            <h2>Admin Profile</h2>
+        <div className="max-w-screen-xl mx-auto px-2 md:px-10 lg:px-20">
+            <div>
+                <div className="p-12 mt-10 justify-center items-center sm:flex sm:space-x-6 bg-[#0060efb7] shadow-xl transform transition-transform hover:scale-105 rounded-md text-white">
+                    <div className="flex w-full mb-6 h-44 sm:h-32 sm:w-32 sm:mb-0">
+                        <img src={user?.photoURL} alt="" className="object-cover w-full h-full rounded-full" />
+                    </div>
+                    <div className="flex flex-col space-y-4">
+                        <div>
+                            <div className="flex gap-2 items-center">
+                                <h1 className="text-xl font-semibold">Name:</h1>
+                                <h2 className="text-xl font-semibold">{user?.displayName}</h2>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <h1 className="text-xl font-semibold">Email:</h1>
+                                <span className="text-xl">{user?.email}</span>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <h1 className="text-xl font-semibold">Role:</h1>
+                                <p className="font-semibold">{userRole?.role ? userRole.role : ""}</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
